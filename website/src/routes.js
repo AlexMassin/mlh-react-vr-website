ad4985d984fc1174fa7dc670b601d4dffd22fe2f
Alex Massin <aptmassin@gmail.com>
2019-05-18 11:19:12 -0400

    changes

--- website/src/routes.js
+++ website/src/routes.js
@@ -3,13 +3,15 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Login from './views/Login';
 import VysitorPortal from './views/VysitorPortal';
 import TravellerDashboard from './views/TravellerDashboard';
+import SetRoute from './views/VysitorPortal';
 
 const Routes = () => (
     <BrowserRouter>
     <Switch>
         <Route exact path='/' component={Login} />
-        <Route path='/vysitor-home' component={VysitorPortal} />
+        <Route path='/vysitor-destinations' component={VysitorPortal} />
         <Route path='/traveller-dashboard' component={TravellerDashboard} />
+        <Route path='/set-route' component={SetRoute} />
     </Switch>
     </BrowserRouter>
 
